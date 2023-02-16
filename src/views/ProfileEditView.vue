6f80d501251f3255178d3620f49495706d59308c
Raygud <runigudmundarson@hotmail.com>
2023-02-16 21:33:01 +0100

    Review editing functionaly finished

---
+++ src/views/ProfileEditView.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+
+        <ReviewEdit />
+
+</div>
+</template>
+
+<script>
+
+
+
+
+
+import ReviewEdit from '@/components/ReviewEdit.vue';
+
+export default {
+    setup() {
+        return {};
+    },
+    components: { ReviewEdit }
+}
+</script>
+
+<style lang="scss" scoped></style>
